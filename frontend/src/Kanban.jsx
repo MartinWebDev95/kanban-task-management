98cf46e11e0b6bfdef81d62346b42373cb0606c7
Juan Antonio <jamll.muro@gmail.com>
2023-02-14 09:24:56 +0100

    Add new component Sidebar with the list of boards

--- frontend/src/Kanban.jsx
+++ frontend/src/Kanban.jsx
@@ -1,8 +1,15 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main className="flex">
+        <Sidebar />
+        <section className="bg-gray-100 dark:bg-slate-900 h-[88vh] flex-1" />
+      </main>
+    </>
   );
 }
 
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,54 @@
+function Sidebar() {
+  return (
+    <aside className="bg-white dark:bg-slate-800 w-[20%] pt-8">
+      <div className="flex flex-col justify-between">
+        <h2 className="uppercase text-gray-500 font-semibold ml-5 text-sm tracking-widest">
+          All boards (3)
+        </h2>
+
+        <ul className="mt-4 mr-4 font-semibold">
+          <li>
+            <button
+              type="button"
+              className="flex items-center gap-4 pl-5 py-4 rounded-r-full w-full text-gray-500 lg:hover:text-indigo-700 dark:lg:hover:bg-white lg:hover:bg-gray-100 transition-all duration-200 ease-in-out"
+            >
+              <img src="/assets/icon-board.svg" alt="Board" />
+
+              <span>
+                Platform Launch
+              </span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="flex items-center gap-4 pl-5 py-4 rounded-r-full w-full text-gray-500 lg:hover:text-indigo-700 dark:lg:hover:bg-white lg:hover:bg-gray-100 transition-all duration-200 ease-in-out"
+            >
+              <img src="/assets/icon-board.svg" alt="Board" />
+
+              <span>
+                Marketing Plan
+              </span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="flex items-center gap-4 pl-5 py-4 rounded-r-full w-full text-gray-500 lg:hover:text-indigo-700 dark:lg:hover:bg-white lg:hover:bg-gray-100 transition-all duration-200 ease-in-out"
+            >
+              <img src="/assets/icon-board.svg" alt="Board" />
+
+              <span>
+                Roadmap
+              </span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;
