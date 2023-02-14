b9c085a72a703473ed40f94b3e18d0f53da4ccc4
Juan Antonio <jamll.muro@gmail.com>
2023-02-14 09:37:18 +0100

    Add new ToggleTheme component with a custom checkbox to choose between dark or light theme

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
+import ToggleTheme from './ToggleTheme';
+
 function Sidebar() {
   return (
-    <aside className="bg-white dark:bg-slate-800 w-[20%] pt-8">
-      <div className="flex flex-col justify-between">
+    <aside className="bg-white dark:bg-slate-800 w-[20%] py-8 flex flex-col justify-between">
+      <div>
         <h2 className="uppercase text-gray-500 font-semibold ml-5 text-sm tracking-widest">
           All boards (3)
         </h2>
@@ -46,6 +48,10 @@ function Sidebar() {
             </button>
           </li>
         </ul>
+      </div>
+
+      <div className="w-full">
+        <ToggleTheme />
       </div>
     </aside>
   );
