d22c9379c9889bbd82f2edb1a6a1bbee546fae01
Juan Antonio <jamll.muro@gmail.com>
2023-02-16 09:14:19 +0100

    Change height of modal AddNewTask

--- frontend/src/components/AddNewTask.jsx
+++ frontend/src/components/AddNewTask.jsx
@@ -16,7 +16,7 @@ function AddNewTask({ addNewTask, setAddNewTask }) {
   return (
     addNewTask && (
       <div
-        className="grid place-items-center bg-black/50 absolute z-30 top-0 left-0 bottom-0 w-full h-full py-8"
+        className="grid place-items-center bg-black/50 absolute z-30 top-0 left-0 bottom-0 w-full h-screen py-8"
         aria-label="newTask-modal"
         onClick={handleCloseNewTaskModal}
       >
