5b7f7c1c76da553119656a9043e5e76bdac5a133
Juan Antonio <jamll.muro@gmail.com>
2023-02-17 09:11:08 +0100

    Change styles of ListOfBoards component

--- frontend/src/components/ListOfBoards.jsx
+++ frontend/src/components/ListOfBoards.jsx
@@ -2,7 +2,7 @@ import Board from './Board';
 
 function ListOfBoards({ boards }) {
   return (
-    <ul className="mt-4 mr-4 font-semibold w-full">
+    <ul className="mt-4 pr-4 font-semibold w-full">
       {boards.map(({ board }) => (
         <Board board={board} />
       ))}
