d7b79029865f27cd7a708001ec9ac8dada74bea4
Juan Antonio <jamll.muro@gmail.com>
2023-02-20 12:56:14 +0100

    Add new useAuthContext custom hook to use the auth context easier

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Kanban from './Kanban';
 import './index.css';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <Kanban />
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <Kanban />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
