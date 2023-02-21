9923f08c6de24dc7f00789d11eb2a5958d0b64fd
Juan Antonio <jamll.muro@gmail.com>
2023-02-21 11:10:12 +0100

    Add new route to register page

--- frontend/src/Kanban.jsx
+++ frontend/src/Kanban.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import AuthLayout from './components/AuthLayout';
+import AuthLayout from './layout/AuthLayout';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/">
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route
           index
           element={(
