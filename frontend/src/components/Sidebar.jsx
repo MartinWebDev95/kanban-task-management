b976093db077aa6c07163d8af0f4c61e34a838ea
Juan Antonio <jamll.muro@gmail.com>
2023-02-22 11:09:10 +0100

    Add useEffect that changes every time that a new board is added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,26 +7,25 @@ import ToggleTheme from './ToggleTheme';
 import Logout from './Logout';
 import useAuthContext from '../hooks/useAuthContext';
 import getAllBoards from '../services/getAllBoards';
+import AddNewBoardModal from './AddNewBoardModal';
 
 function Sidebar({ showSidebar, setShowSidebar }) {
   const [hideSidebar, setHideSidebar] = useState(!!localStorage.getItem('hideSidebar'));
+  const [openNewBoard, setOpenNewBoard] = useState(false);
   const [boards, setBoards] = useState([]);
   const [loading, setLoading] = useState(true);
   const { currentUser } = useAuthContext();
 
   useEffect(() => {
     getAllBoards({ userId: currentUser.uid })
-      .then((query) => {
-        query.forEach((document) => {
-          if (boards.length > 0) {
-            setBoards([...boards, { uid: document.id, name: document.data().name }]);
-          } else {
-            setBoards([{ uid: document.id, name: document.data().name }]);
-          }
-        });
+      .then((allBoards) => {
+        setBoards(allBoards);
+      }).catch((err) => {
+        throw new Error(err.message);
+      }).finally(() => {
         setLoading(false);
       });
-  }, [currentUser]);
+  }, [boards]);
 
   const handleCloseModalSidebar = (e) => {
     if (e.target.ariaLabel === 'sidebar-modal') {
@@ -35,38 +34,42 @@ function Sidebar({ showSidebar, setShowSidebar }) {
   };
 
   return (
-    <aside
-      className={` 
+    <>
+      <aside
+        className={` 
         ${showSidebar
-        ? 'grid place-items-center bg-black/50 absolute w-full lg:flex'
-        : 'hidden lg:flex'}
+          ? 'grid place-items-center bg-black/50 absolute w-full lg:flex'
+          : 'hidden lg:flex'}
         ${hideSidebar
-          ? 'lg:w-[0vw] lg:fixed'
-          : 'lg:flex lg:fixed lg:w-[20vw]'} 
+            ? 'lg:w-[0vw] lg:fixed'
+            : 'lg:flex lg:fixed lg:w-[20vw]'} 
         top-0 left-0 h-full
       `}
-      aria-label="sidebar-modal"
-      onClick={handleCloseModalSidebar}
-    >
-      <div className="bg-white dark:bg-slate-800 rounded-xl w-4/5 py-4 lg:w-full lg:h-full lg:rounded-none lg:flex lg:flex-col lg:justify-between lg:pt-20">
+        aria-label="sidebar-modal"
+        onClick={handleCloseModalSidebar}
+      >
+        <div className="bg-white dark:bg-slate-800 rounded-xl w-4/5 py-4 lg:w-full lg:h-full lg:rounded-none lg:flex lg:flex-col lg:justify-between lg:pt-20">
 
-        <div className={`lg:mt-5 ${hideSidebar && 'lg:overflow-hidden'}`}>
-          <h2 className="uppercase text-gray-500 font-semibold ml-6 text-sm tracking-widest">
-            {`All boards (${boards.length})`}
-          </h2>
+          <div className={`lg:mt-5 ${hideSidebar && 'lg:overflow-hidden'}`}>
+            <h2 className="uppercase text-gray-500 font-semibold ml-6 text-sm tracking-widest">
+              {`All boards (${boards.length})`}
+            </h2>
 
-          {!loading && (<ListOfBoards boards={boards} />)}
+            {!loading && (<ListOfBoards boards={boards} setOpenNewBoard={setOpenNewBoard} />)}
+          </div>
+
+          <div className={`w-full mt-4 lg:mt-0 ${hideSidebar && 'lg:overflow-hidden'}`}>
+            <Logout />
+
+            <HideSidebarButton hideSidebar={hideSidebar} setHideSidebar={setHideSidebar} />
+
+            <ToggleTheme />
+          </div>
         </div>
+      </aside>
 
-        <div className={`w-full mt-4 lg:mt-0 ${hideSidebar && 'lg:overflow-hidden'}`}>
-          <Logout />
-
-          <HideSidebarButton hideSidebar={hideSidebar} setHideSidebar={setHideSidebar} />
-
-          <ToggleTheme />
-        </div>
-      </div>
-    </aside>
+      <AddNewBoardModal openNewBoard={openNewBoard} setOpenNewBoard={setOpenNewBoard} />
+    </>
   );
 }
 
