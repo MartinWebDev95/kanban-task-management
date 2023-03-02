95e6383693466f2886118df100ff7b33dba9d518
Juan Antonio <jamll.muro@gmail.com>
2023-03-02 09:00:40 +0100

    Add new custom hook useFetchBoards which is listening for changes

---
+++ frontend/src/hooks/useFetchBoards.js
@@ -0,0 +1,43 @@
+import {
+  collection, query, where, doc, orderBy, onSnapshot,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import useAuthContext from './useAuthContext';
+
+function useFetchBoards() {
+  const [boards, setBoards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { currentUser } = useAuthContext();
+
+  useEffect(() => {
+    // Get the reference of the current user
+    const userRef = doc(db, `users/${currentUser.uid}`);
+
+    // Get all boards that belong to the current user order by creation date
+    const q = query(
+      collection(db, 'boards'),
+      where('userRef', '==', userRef),
+      orderBy('timeStamp'),
+    );
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      const collectionBoards = [];
+
+      querySnapshot.forEach((document) => {
+        collectionBoards.push({ uid: document.id, name: document.data().name });
+      });
+
+      setBoards(collectionBoards);
+      setLoading(false);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  return { boards, loading };
+}
+
+export default useFetchBoards;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import Spinner from '../components/Spinner';
+import useFetchBoards from '../hooks/useFetchBoards';
 
 function Home() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const { boards, loading } = useFetchBoards();
 
   return (
-    <>
-      <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-      <main className="flex pt-16">
-        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-        <section className="bg-gray-100 dark:bg-slate-900 h-[88vh] flex-1" />
-      </main>
-    </>
+    loading
+      ? (<Spinner />)
+      : (
+        <>
+          <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+          <main className="flex pt-16">
+            <Sidebar
+              showSidebar={showSidebar}
+              setShowSidebar={setShowSidebar}
+              boards={boards}
+            />
+            <section className="bg-gray-100 dark:bg-slate-900 h-[88vh] flex-1" />
+          </main>
+        </>
+      )
   );
 }
 
