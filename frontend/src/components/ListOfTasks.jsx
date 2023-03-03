8041f0a6f4ae19855936cb4768924fa14c203898
Juan Antonio <jamll.muro@gmail.com>
2023-03-03 12:10:02 +0100

    Change ListOfTasks component to show a message when there are not tasks yet

--- frontend/src/components/ListOfTasks.jsx
+++ frontend/src/components/ListOfTasks.jsx
@@ -1,11 +1,22 @@
 import Spinner from './Spinner';
+import Task from './Task';
 
 function ListOfTasks({ tasks, loadingTasks }) {
   return (
     loadingTasks
       ? (<Spinner />)
       : (
-        tasks.map((task) => console.log(task))
+        tasks.length === 0 ? (
+          <p className="grid place-items-center h-full">Create your first task</p>
+        ) : (
+          <ul className="flex flex-col gap-4">
+            {tasks.map((task) => (
+              <li>
+                <Task key={task.uid} task={task} />
+              </li>
+            ))}
+          </ul>
+        )
       )
   );
 }
