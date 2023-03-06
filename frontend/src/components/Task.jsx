ce5378ffc082c70303e593ab014d7e372dec56f4
Juan Antonio <jamll.muro@gmail.com>
2023-03-06 10:14:12 +0100

    Change Task component to button

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,8 +1,14 @@
 function Task({ task }) {
+  const handleClick = (e) => {};
+
   return (
-    <button type="button" className="dark:bg-slate-800 p-4 rounded-lg flex flex-col gap-2">
+    <button
+      type="button"
+      className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg"
+      onClick={handleClick}
+    >
       <h2 className="dark:text-white text-black font-semibold">{task.title}</h2>
-      <p className="dark:text-slate-500 font-semibold text-sm">0 of 1 subtasks</p>
+      <p className="text-gray-500 font-semibold text-sm">0 of 1 subtasks</p>
     </button>
   );
 }
