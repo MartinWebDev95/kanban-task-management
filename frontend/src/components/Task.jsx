74c5e980c706c1770d1b6f32ab02e6aa85a23fd2
Juan Antonio <jamll.muro@gmail.com>
2023-03-06 10:36:17 +0100

    Change styles of Task component

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -4,10 +4,10 @@ function Task({ task }) {
   return (
     <button
       type="button"
-      className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg"
+      className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg group"
       onClick={handleClick}
     >
-      <h2 className="dark:text-white text-black font-semibold">{task.title}</h2>
+      <h2 className="dark:text-white text-black font-semibold group-hover:text-indigo-700 transition-all duration-150 ease-in-out">{task.title}</h2>
       <p className="text-gray-500 font-semibold text-sm">0 of 1 subtasks</p>
     </button>
   );
