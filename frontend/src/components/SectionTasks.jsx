1d388dc564ec6e47bab4ad37f516b8a54c53a0fd
Juan Antonio <jamll.muro@gmail.com>
2023-03-13 09:32:01 +0100

    Fix height size in SectionTask component

--- frontend/src/components/SectionTasks.jsx
+++ frontend/src/components/SectionTasks.jsx
@@ -3,7 +3,7 @@ import Spinner from './Spinner';
 
 function SectionTasks({ tasks, loadingTasks }) {
   return (
-    <section className="bg-gray-100 dark:bg-slate-900 w-full px-4 pt-8 lg:overflow-y-scroll">
+    <section className="bg-gray-100 dark:bg-slate-900 w-full px-4 pt-8 h-[88vh] scrollbar-hide overflow-y-scroll">
       {loadingTasks
         ? (<Spinner />)
         : (<ListOfTasks tasks={tasks} />)}
