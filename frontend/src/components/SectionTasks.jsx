628b92a251c8e33e22eb79d3b99e18bf593d08a8
Juan Antonio <jamll.muro@gmail.com>
2023-03-14 14:31:29 +0100

    Change some styles in SectionTasks component and Task component

--- frontend/src/components/SectionTasks.jsx
+++ frontend/src/components/SectionTasks.jsx
@@ -3,7 +3,7 @@ import Spinner from './Spinner';
 
 function SectionTasks({ taskStatus, loadingTaskStatus, selectedBoard }) {
   return (
-    <section className="bg-gray-100 dark:bg-slate-900 w-full px-4 pt-8 h-[88vh] scrollbar-hide overflow-scroll">
+    <section className="bg-gray-100 dark:bg-slate-900 px-4 pt-8 h-[88vh] scrollbar-hide overflow-y-scroll overflow-x-scroll">
       {loadingTaskStatus
         ? (<Spinner />)
         : (<ListOfStatusTasks taskStatus={taskStatus} selectedBoard={selectedBoard} />)}
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -14,10 +14,10 @@ function Task({ task }) {
     <>
       <button
         type="button"
-        className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg group"
+        className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg group w-full"
         onClick={handleClick}
       >
-        <h2 className="dark:text-white text-black font-semibold group-hover:text-indigo-700 transition-all duration-150 ease-in-out">{task.title}</h2>
+        <h2 className="dark:text-white text-black font-semibold text-left group-hover:text-indigo-700 transition-all duration-150 ease-in-out">{task.title}</h2>
         <p className="text-gray-500 font-semibold text-sm">
           {`${subtasks.filter((subtask) => subtask.doneSubtask).length} of ${subtasks.length} subtasks`}
         </p>
